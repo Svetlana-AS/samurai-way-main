5d25773ccf2d308073c4f852daef8380a315cd9d
Svetlana-AS <astaninasv1@gmail.com>
2023-02-27 14:54:35 +0100

    Import export of components. Lesson 9Ñ

---
+++ src/Tehnologies.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+function Tehnologies() {
+    return (
+        <div>
+            <ul>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>);
+}
+
+
+export default Tehnologies;
